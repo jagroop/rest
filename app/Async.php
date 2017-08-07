60ad58533aa946a5af1508a7b40dfb6d630cd2ad
Jagroop Singh <geek.jagroop@gmail.com>
2017-08-07 09:10:09 +0530

    :art:

--- app/Async.php
+++ app/Async.php
@@ -2,14 +2,12 @@
 namespace App;
 
 use App\Artisan\Rest;
-use App\Artisan\Mail;
-use App\Artisan\Notification;
 
 class Async extends Rest {
 
 	public function sendEmailsToAll() {
 		$offerId = request('offer_id');
-    app_log('test');
+		app_log('test');
 		//...
 	}
 }
